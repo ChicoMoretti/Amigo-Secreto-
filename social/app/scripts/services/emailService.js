9840aae31b1e2f09ead7361254266aa172c048ef
Chico Moretti <chicomoretti@gmail.com>
2015-05-25 10:00:07 -0300

    Revert "Amigo - Secreto"
    This reverts commit 5f05527919ce1f4c4cdae81459f656a905411d7a.

--- social/app/scripts/services/emailService.js
+++
@@ -1,6 +0,0 @@
-angular.module('amigo-secreto')
-    .factory('emailService', ['$resource', function ($resource) {
-
-        return $resource($SERVICES_CONTEXT + 'email/:params', {}, {/*codigão aqui*/});
-
-    }]);
--- social/app/scripts/services/sorteioService.js
+++
@@ -1,6 +0,0 @@
-angular.module('amigo-secreto')
-    .factory('sorteioService', ['$resource', function ($resource) {
-
-        return $resource($SERVICES_CONTEXT + 'sorteio/:params', {}, {/*codigão aqui*/});
-
-    }]);
